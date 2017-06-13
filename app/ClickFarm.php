9da957b94164d93fb9b61383d8d3d784e7635f1d
xyz3282836 <ruizhouliu@163.com>
2017-06-13 19:27:38 +0800

    fix bug

--- app/ClickFarm.php
+++ app/ClickFarm.php
@@ -46,4 +46,51 @@ class ClickFarm extends Model
         $text = self::OUT_TEXT;
         return $text[$this->status];
     }
+
+    const SITE_US = 1;//美国
+    const SITE_CA = 2;//加拿大
+    const SITE_UK = 3;//英国
+    const SITE_DE = 4;//德国
+    const SITE_FR = 5;//法国
+    const SITE_JP = 6;//日本
+    const SITE_IN = 7;//
+    const SITE_ES = 8;//
+    const SITE_NL = 9;//
+    const SITE_IT = 10;//
+
+    public static function getCurrency($site){
+        switch ($site){
+            case self::SITE_US:
+                return 'us';
+                break;
+            case self::SITE_CA:
+                return 'ca';
+                break;
+            case self::SITE_UK:
+                return 'uk';
+                break;
+            case self::SITE_DE:
+                return 'de';
+                break;
+            case self::SITE_FR:
+                return 'fr';
+                break;
+            case self::SITE_JP:
+                return 'jp';
+                break;
+            case self::SITE_IN:
+                return 'in';
+                break;
+            case self::SITE_ES:
+                return 'es';
+                break;
+            case self::SITE_NL:
+                return 'nl';
+                break;
+            case self::SITE_IT:
+                return 'it';
+                break;
+        }
+    }
+
 }
