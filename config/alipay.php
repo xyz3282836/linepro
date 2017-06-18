2362c5fafe1d0b36336c5e01a4a0145a7c98893c
xyz3282836 <ruizhouliu@163.com>
2017-06-18 12:05:54 +0800

    fix bug

--- config/alipay.php
+++ config/alipay.php
@@ -1,14 +1,14 @@
 <?php
 
-return [
-    'env'               => 'sandbox',
-    'sign_type'         => 'RSA2',
-    'app_id'            => '',
-    'return_url'        => '',
-    'notify_url'        => '',
-    'app_private_key'   => '',
-    'alipay_public_key' => ''
-];
+//return [
+//    'env'               => 'sandbox',
+//    'sign_type'         => 'RSA2',
+//    'app_id'            => '',
+//    'return_url'        => '',
+//    'notify_url'        => '',
+//    'app_private_key'   => '',
+//    'alipay_public_key' => ''
+//];
 
 
 
