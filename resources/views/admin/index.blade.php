7b3f8d935d7a361eb8012135bc62c0f1cecb7cf0
zhou <ruizhouliu@163.com>
2017-06-25 08:17:15 +0800

    fix bug

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -32,7 +32,12 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
+    <style>
+        table td{
+            max-width: 300px!important;
+            word-wrap: break-word
+        }
+    </style>
 </head>
 
 <body class="hold-transition {{config('admin.skin')}} {{join(' ', config('admin.layout'))}}">
