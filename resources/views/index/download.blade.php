6d7c8d012c1b0c2e77b7878977b3d822cbcacc09
xyz3282836 <ruizhouliu@163.com>
2017-06-28 16:56:27 +0800

    去除移动端显示

--- resources/views/index/download.blade.php
+++ resources/views/index/download.blade.php
@@ -49,13 +49,13 @@
             <div class="navbar-header">
 
                 <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                        data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
+                {{--<button type="button" class="navbar-toggle collapsed" data-toggle="collapse"--}}
+                        {{--data-target="#app-navbar-collapse">--}}
+                    {{--<span class="sr-only">Toggle Navigation</span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                    {{--<span class="icon-bar"></span>--}}
+                {{--</button>--}}
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="javascript:;">
