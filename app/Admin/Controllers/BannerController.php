796beb398ae899e0c90943bb91354cab652f9f27
xyz3282836 <ruizhouliu@163.com>
2017-08-15 14:38:11 +0800

    fix bug

--- app/Admin/Controllers/BannerController.php
+++ app/Admin/Controllers/BannerController.php
@@ -54,7 +54,7 @@ class BannerController extends Controller
     public function form($type = 1)
     {
         return Admin::form(Banner::class, function (Form $form) use ($type) {
-            $form->radio('type', '图片类型')->options(['1' => 'Banner(1920x600)', '2' => 'Logo(100x50)', '3' => '购物车页banner', '4' => '新建页面banner', '首页图片(1920x600)' => '5', '首页logo(100x50)' => '6'])->default('1')->rules('required');
+            $form->radio('type', '图片类型')->options(['1' => 'Banner(1920x600)', '2' => 'Logo(100x50)', '3' => '购物车页banner', '4' => '新建页面banner', '5' => '首页图片(1920x600)', '6' => '首页logo(100x50)'])->default('1')->rules('required');
             switch ($type) {
                 case 1:
                     $form->text('title', '图片标题')->default('banner')->rules('required');
