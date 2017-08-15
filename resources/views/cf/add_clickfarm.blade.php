aece1cd12440323f5b1277f67555917194170bb2
xyz3282836 <ruizhouliu@163.com>
2017-08-15 17:17:46 +0800

    fix bug

--- resources/views/cf/add_clickfarm.blade.php
+++ resources/views/cf/add_clickfarm.blade.php
@@ -170,8 +170,8 @@
                             </div>
 
                             <div class="form-group">
-                                <label class="col-md-4 control-label"> 所需<img width="15" src="/img/gold.png" />(1<img width="15" src="/img/gold.png" />=0.01元)</label>
-                                <label class="col-md-6 control-label"><span v-text="getservice"></span><img width="15" src="/img/gold.png" /></label>
+                                <label class="col-md-4 control-label"> 所需<img width="15" src="/img/gold.png" /></label>
+                                <label class="col-md-6 control-label"><span v-text="getservice"></span><img width="15" src="/img/gold.png" />(1<img width="15" src="/img/gold.png" />=0.01元)</label>
                                 <label class="col-md-1 control-label">
                                     <a href="{{url('faqs')}}" target="_blank">?</a>
                                 </label>
