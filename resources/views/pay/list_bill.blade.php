ff958094fa0cd7feb2676e242972506acd7112dc
xyz3282836 <ruizhouliu@163.com>
2017-08-22 11:53:43 +0800

    fix bug

--- resources/views/pay/list_bill.blade.php
+++ resources/views/pay/list_bill.blade.php
@@ -9,7 +9,7 @@
 @endsection
 
 @section('content')
-    <div class="container">
+    <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-default">
