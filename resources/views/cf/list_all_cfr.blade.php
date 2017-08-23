639089336330ea4e9623f7896c8effa193934646
zhou <ruizhouliu@163.com>
2017-08-23 22:44:39 +0800

    修改时区

--- resources/views/cf/list_all_cfr.blade.php
+++ resources/views/cf/list_all_cfr.blade.php
@@ -155,7 +155,7 @@
                 app.star = estatus > 1?star:0;
                 app.title = title;
                 app.content = content;
-                $('#eform').validator('update')
+                $('#eform').validator('validate')
             });
 
             $('#eform').validator().on('submit', function (e) {
--- resources/views/cf/list_cf_result.blade.php
+++ resources/views/cf/list_cf_result.blade.php
@@ -158,7 +158,7 @@
                 app.star = estatus > 1?star:0;
                 app.title = title;
                 app.content = content;
-                $('#eform').validator('update')
+                $('#eform').validator('validate')
             });
 
             $('#eform').validator().on('submit', function (e) {
