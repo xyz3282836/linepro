5aed8106e3d6cd6665010c39951fb1d0e896f91e
zhou <ruizhouliu@163.com>
2017-08-25 21:52:10 +0800

    修改时区

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,8 +87,8 @@
         .panel-default {
             border-color: rgb(222,236,245);
         }
-        table th,table td{
-            border-color: rgb(222,236,245)!important;
+        .table-bordered, .table-bordered>tbody>tr>td, .table-bordered>tbody>tr>th, .table-bordered>tfoot>tr>td, .table-bordered>tfoot>tr>th, .table-bordered>thead>tr>td, .table-bordered>thead>tr>th {
+            border: 1px solid rgb(222,236,245);
         }
     </style>
     @yield('css')
